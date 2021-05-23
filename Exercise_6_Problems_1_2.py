1739c260866efe2b0f470e34a4ed6ca09871b17b
YoshitoKonno <>
2021-05-23 13:33:20 +0000

    I finished Exercise_6_Problems_1_2.py

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -18,6 +18,9 @@ data = None
 
 # YOUR CODE HERE 1
 
+# Read the data file. skip the second row and convert the no-data values into NaN.
+data=pd.read_csv('data/1091402.txt',skiprows=[1],delim_whitespace=True,na_values=-9999)
+
 # ### Part 2 
 # 
 # In this section, you will calculate simple statistics based on the input data:
@@ -26,8 +29,20 @@ data = None
 #     - Assign your answer to a variable called `tavg_nodata_count`.
 
 tavg_nodata_count = None
+
 #YOUR CODE HERE 2
 
+#variable 'a' means all columns of 'TAVG'
+#tavg_nodata_count means count of nodata of 'TAVG'. 
+a=data['TAVG']
+tavg_nodata_count=a.isnull().sum()
+
+#for i in range(tavg.length()):
+ #if tavg[i]==-9999:
+  # tavg_nodata_count=tavg_nodata_count+1
+
+
+ 
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -39,7 +54,14 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 #     - Assign your answer into a variable called `tmin_nodata_count`
 
 tmin_nodata_count = None
+
 #YOUR CODE HERE 3
+
+#variable 'b' means all columns of 'TMIN'
+#tmin_nodata_count means count of nodata of 'TMIN'.
+
+b=data['TMIN']
+tmin_nodata_count=b.isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -52,7 +74,11 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
+#variable 'c' means all columns of 'DATE'
+#day_count means count of date 
 
+c=data['DATE']
+day_count=c.count()
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print("Number of days:", day_count)
@@ -66,6 +92,9 @@ first_obs = None
  
 # YOUR CODE HERE 5
 
+#first_obs means first columns of 'DATE'
+first_obs = data['DATE'][0]
+
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Date of the first observation:',first_obs)
@@ -77,6 +106,9 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
+
+#last_obs means last columns of 'DATE'
+last_obs = data['DATE'][len(data['DATE'])-1]
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -91,6 +123,15 @@ avg_temp = None
 
 # YOUR CODE HERE 7
 
+
+sum_temp=0#sum_temp mean total of tempature but it doesn't include nodata
+for i in range(len(data['TAVG'])-1):
+  #if "data['TAVG'][i]" is not nodata, we add "data['TAVG'][i]" to sum_temp
+  if(np.isnan(data['TAVG'][i])==False):
+   sum_temp+=data['TAVG'][i]
+#avg_temp means average tempature for the whole data file from column 'TAVG'  
+avg_temp=sum_temp/(len(data['TAVG'])-tavg_nodata_count)
+
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
@@ -104,6 +145,15 @@ avg_temp_1969 = None
 
 # YOUR CODE HERE 8
 
+sum_temp_1969=0##sum_temp mean total of tempature in 1969 5/1~8/31 but it doesn't include nodata
+sum_temp_1969_count=0#total of data in 1969 5/1~8/31
+for i in range(len(data['TMAX'])-1):
+  if(np.isnan(data['TMAX'][i])==False)and(data['DATE'][i]>=19690501)and(data['DATE'][i]<=19690831):
+    sum_temp_1969+=data['TMAX'][i]
+    sum_temp_1969_count+=1
+#avg_temp_1969 means average tempature for the 1969 5/1~8/31 from column 'TAVG'
+avg_temp_1969=sum_temp_1969/sum_temp_1969_count
+
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print a number
 print('Average temperature (F) for the Summer of 69:', round(avg_temp_1969, 2))
@@ -116,6 +166,38 @@ print('Average temperature (F) for the Summer of 69:', round(avg_temp_1969, 2))
 monthly_data = None
 
 # YOUR CODE HERE 9
+
+month=[]#month[i] means month of data['DATE'][i]
+celsius_temp=[]#celsius_temp[i] means tempature of data['TAVG'] #convert Fahrenheit to Celsius 
+total_temp=0#we use this variable to caluculate total of tempature each month  
+day_count=0# we use this variable to count "day"
+month_ave=[]#month_ave means data of all "total_temp/day_count"
+
+for i in range(len(data['DATE'])-1):
+  month.append(int(data['DATE'][i]%10000/100))
+  celsius_temp.append((data['TAVG'][i]-32.0)/1.8)
+
+
+
+for i in range(len(data['DATE'])-1):
+  if(i==0):#in case, i==0
+    total_temp+=celsius_temp[i]
+    day_count+=1
+  else:
+    if(month[i-1]!=month[i]):
+      month_ave.append(total_temp/day_count)
+      total_temp=celsius_temp[i]
+      day_count=1 #initialization of day_count
+    else:
+      total_temp+=celsius_temp[i]
+      day_count+=1
+month_ave.append(total_temp/day_count)
+monthly_data=pd.DataFrame({'temp_celsius':month_ave})
+      
+
+
+   
+
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print the length of variable monthly_data
